18b29fd0d52f69a7ed24464b46a14a930fa59fea
Nick Reardon <32754140+ThingOfNightmare@users.noreply.github.com>
2020-02-09 21:43:15 -0800

    Add project files.

---
+++ CS1D AS4 DequeToQueue/main.cpp
@@ -0,0 +1,27 @@
+/*****************************************************************************
+ * AUTHOR 	  		: Nick Reardon
+ * Assignment #4	: Deque To Queue
+ * CLASS			: CS1D
+ * SECTION			: MW - 2:30p
+ * DUE DATE			: 02 / 10 / 20
+ *****************************************************************************/
+#include "main.h"
+
+using std::cout; using std::endl;
+
+
+int main()
+{
+
+	/*
+	 * HEADER OUTPUT
+	 */
+	PrintHeader(cout, "Prompt.txt");
+
+	/********************************************************************/
+
+
+
+	system("pause");
+	return 0;
+}
